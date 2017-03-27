2db6b45ceb141496ee9ca86e7a468760ed615546
Fern <fernanlukban@gmail.com>
2017-03-26 22:04:42 -0700

    trying

--- bot.js
+++ bot.js
@@ -9,6 +9,15 @@ client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('Pong!');
   }
+  if (msg.content.startsWith('$')){
+  	doAction(msg)
+  }
 });
+
+function doAction(msg){
+	if msg.content.startsWith('$hello'){
+		msg.reply('lol')
+	}
+};
 
 client.login('MjkzMjU5NjQ4OTE0Njg1OTU0.C7oreg.8MWVuxlpgjU9hpU8290eYqHy-ZM');
