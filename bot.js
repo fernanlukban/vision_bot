ce4d32d244d6aceb839d19c41d21d7fdccde08f6
Fern <fernanlukban@gmail.com>
2017-03-26 23:51:47 -0700

    started tournament adding

--- bot.js
+++ bot.js
@@ -4,7 +4,6 @@ const commandPrefix = '$';
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`);
-  console.log(client.guilds);
 });
 
 client.on('message', msg => {
@@ -14,9 +13,56 @@ client.on('message', msg => {
 });
 
 function doAction(msg){
-	if (msg.content.startsWith(commandPrefix + 'hello')){
-		msg.reply('lol')
+
+	content = msg.content.split(" ");
+	command = content[0].slice(commandPrefix.length);
+	args = content.slice(1);
+
+	console.log(command, args);
+
+	if (command === 'ping'){
+		msg.reply('pong');
+	} else if (command === '$init'){
+		numOfTeams = args[0]
+		startTournament(msg, numOfTeams);
+	} else if (command === '$delete'){
+		deleteAllChannels(msg);
 	}
 };
+
+
+function startTournament(msg, numOfTeams){
+	createChannels(msg, numOfTeams);
+}
+
+function createChannels(msg, numOfTeams){
+	var channelsCreated = new Array();
+	for (var channel of msg.guild.channels){
+		//console.log(RegExp(`Team {1,${numOfTeams}}`).test(channel[1].name));
+		//if (channel instanceof Discord.VoiceChannel){
+			if (RegExp(`Team {1,${numOfTeams}}`).test(channel[1].name)){
+				var number = Number(channel[1].name.split(" ")[1]);
+				if (!channelsCreated.includes(number)){
+					channelsCreated.push(number);
+				}
+				console.log(`Team ${number} channel found!`);
+			}
+		//}
+	}
+	//console.log(channelsCreated);
+	for (var i = 1; i <= numOfTeams; i++){
+		if (!channelsCreated.includes(i)){
+			msg.guild.createChannel(`Team ${i}`, 'voice');
+			console.log(`Team ${i} channel created!`);
+		}
+	}
+}
+
+function deleteAllChannels(msg){
+	for (var channel of msg.guild.channels){
+		//console.log(channel);
+		channel[1].delete();
+	}
+}
 
 client.login('MjkzMjU5NjQ4OTE0Njg1OTU0.C7oreg.8MWVuxlpgjU9hpU8290eYqHy-ZM');
