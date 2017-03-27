9825a0adc75f3ed39e248d1132e6426b690d93a9
Fern <fernanlukban@gmail.com>
2017-03-26 22:29:22 -0700

    test

--- bot.js
+++ bot.js
@@ -1,21 +1,22 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const commandPrefix = '$';
+
+
+console.log(client.guilds);
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-  if (msg.content.startsWith('$')){
+  if (msg.content.startsWith(commandPrefix)){
   	doAction(msg)
   }
 });
 
 function doAction(msg){
-	if (msg.content.startsWith('$hello')){
+	if (msg.content.startsWith(commandPrefix + 'hello')){
 		msg.reply('lol')
 	}
 };
